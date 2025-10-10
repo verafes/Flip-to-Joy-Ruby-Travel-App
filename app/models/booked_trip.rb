6229e55cd22fefca2e74953a30fe6eabe21978ba
verafes <verafes@gmail.com>
2025-10-09 21:39:33 -0700

    fix status for booked trips

--- app/models/booked_trip.rb
+++ app/models/booked_trip.rb
@@ -2,6 +2,12 @@ class BookedTrip < ApplicationRecord
   belongs_to  :trip
   belongs_to  :traveler, class_name: 'User', foreign_key: :user_id
   has_many :payments, dependent: :destroy
+
+  enum :status, {
+      pending_payment: 0,
+      paid: 1,
+      cancelled: 2
+    }
   
   validates :trip_id, uniqueness: { scope: :user_id, 
                                     message: "You have already booked this trip." }
